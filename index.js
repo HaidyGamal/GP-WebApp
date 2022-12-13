90a5183a978950906218af23c56393cc67fa818a
Abdelrahman Mohamed <abdelrahman181181@yahoo.com>
2022-12-13 17:23:20 +0200

    testing API first sample

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 // to override methods
 const methodOverride = require('method-override');
+// to use our API with the help of axios
+const axios = require("axios");
 // making /views folder accesable from any path
 app.set('views', path.join(__dirname, '/views'));
 // to include EJS
@@ -31,7 +33,25 @@ app.get(
 app.get(
     '/webVersion',
     (req, res)=>{
-        res.render('pages/webVersion.ejs');
+        let stops = [];
+        const options = {
+          method: 'GET',
+          url: 'https://samplepublictransportationsapi.onrender.com/',
+          headers: {
+            'Accept-Encoding': 'null'
+          }
+        };
+        axios.request(options).then(function (response) {
+          for(let i = 0 ; i<response.data.allNodes.length; i++){
+            stops.push(
+              response.data.allNodes[i].name
+            )
+          }
+          res.render('pages/webVersion.ejs', {stops : stops});
+        }).catch(function (error) {
+          console.error(error);
+        });
+        // res.render('pages/webVersion.ejs');
     }
 );
 // contact us page
@@ -49,17 +69,94 @@ app.get(
     }
 );
 // path result page
-app.get(
+app.post(
     '/webVersion/result',
     (req, res)=>{
-        res.render('pages/result.ejs');
+      let location = req.body.Location
+      let destination = req.body.Destination
+      const options = {
+        method: 'POST',
+        url: 'https://samplepublictransportationsapi.onrender.com/showResult',
+        headers: {
+          'content-type': 'application/json',
+          'Accept-Encoding': 'null'
+        },
+        data: `{"Location":"${location}","Destination":"${destination}"}`
+      };
+      
+      axios.request(options).then(function (response) {
+        let numberOfAvailablePaths = response.data.recordsLength
+        let numberOfPathStops = response.data.length
+        let lastStop = response.data.endNode
+        let routeNumber = 1
+        let allPathsCombined = []
+        let routes = []
+        // to store all paths in one continous array without filtering each route separately
+        for(let i = 0 ; i< numberOfAvailablePaths ; i++){
+          for(let j = 0 ; j< numberOfPathStops ; j++){
+            allPathsCombined.push({
+              name: response.data.routes[i][j].name,
+              lineNumber: response.data.routes[i][j].lineNumber
+            })
+          }
+        }
+        // filtering routes from the collected allPathsCombined array
+        let k = numberOfPathStops
+        for(let i = 0 ; i < allPathsCombined.length ; i = i+numberOfPathStops){
+          routes.push(allPathsCombined.slice(i , k))
+          k = k + numberOfPathStops
+        }
+        res.render('pages/result.ejs' , {location , destination  , routeNumber  , numberOfAvailablePaths });
+      }).catch(function (error) {
+        console.error(error);
+      });
+        // res.render('pages/result.ejs');
     }
 )
 //reult details page
-app.get(
-    '/webVersion/result/resultDetails',
+app.post(
+    '/webVersion/result/resultDetails/:id',
     (req, res)=>{
-        res.render('pages/resultDetails.ejs');
+      const id = req.params.id
+    let location = req.query.Location
+    let destination = req.query.Destination
+    const options = {
+      method: 'POST',
+      url: 'https://samplepublictransportationsapi.onrender.com/showResult',
+      headers: {
+        'content-type': 'application/json',
+        'Accept-Encoding': 'null'
+      },
+      data: `{"Location":"${location}","Destination":"${destination}"}`
+    };
+    
+    axios.request(options).then(function (response) {
+      let numberOfAvailablePaths = response.data.recordsLength
+      let numberOfPathStops = response.data.length
+      let lastStop = response.data.endNode
+      let routeNumber = 1
+      let allPathsCombined = []
+      let routes = []
+      // to store all paths in one continous array without filtering each route separately
+      for(let i = 0 ; i< numberOfAvailablePaths ; i++){
+        for(let j = 0 ; j< numberOfPathStops ; j++){
+          allPathsCombined.push({
+            name: response.data.routes[i][j].name,
+            lineNumber: response.data.routes[i][j].lineNumber
+          })
+        }
+      }
+      // filtering routes from the collected allPathsCombined array
+      let k = numberOfPathStops
+      for(let i = 0 ; i < allPathsCombined.length ; i = i+numberOfPathStops){
+        routes.push(allPathsCombined.slice(i , k))
+        k = k + numberOfPathStops
+      }
+      res.render('pages/resultDetails.ejs' , {id , routes , numberOfPathStops  , lastStop});
+    }).catch(function (error) {
+      console.error(error);
+    });
+        // res.render('pages/resultDetails.ejs');
     }
 );
 //live location page
--- public/js/result.js
+++ public/js/result.js
@@ -1,6 +1,78 @@
-// when i click on the chosen picker route, expand it in details
-const picker = document.querySelector('.picker-window');
+// // when i click on the chosen picker route, expand it in details
+// const picker = document.querySelector('.picker-window');
 
-picker.addEventListener('click', ()=>{
-    location.replace('/webVersion/result/resultDetails');
-});
+// picker.addEventListener('click', ()=>{
+//     location.replace('/webVersion/result/resultDetails');
+// });
+
+
+// var scrollW = document.getElementById("wrap-scroll");
+// var scrollUl = document.getElementById("ul-scroll");
+// var itemsScrolled,
+//     itemsMax,
+//     cloned = false;
+
+// var listOpts = {
+//   itemCount: null,
+//   itemHeight: null,
+//   items: [],
+// };
+
+// function scrollWrap () {
+  
+//   itemsScrolled = Math.ceil( (this.scrollTop + listOpts.itemHeight / 2 ) / listOpts.itemHeight);
+ 
+//   if (this.scrollTop < 1) {
+//     itemsScrolled = 0;
+//   }
+     
+//   listOpts.items.forEach(function (ele) {
+//     ele.classList.remove("active");
+//   })
+  
+//   if (itemsScrolled < listOpts.items.length) {
+//     listOpts.items[itemsScrolled].classList.add("active");
+//   }
+  
+//   if (itemsScrolled > listOpts.items.length - 3) {
+    
+//     var node;
+//     for ( _x = 0; _x <= itemsMax - 1; _x++ ) {
+      
+//       node = listOpts.items[_x];
+      
+//       if ( !cloned ) {
+//         node = listOpts.items[_x].cloneNode(true);
+//       }
+            
+//       scrollUl.appendChild(node);
+//     }
+    
+//     initItems(cloned);
+//     cloned = true; 
+//     itemsScrolled = 0;
+    
+//   }
+// }
+
+// function initItems (scrollSmooth) {
+  
+//   listOpts.items = [].slice.call(scrollUl.querySelectorAll("li"));
+//   listOpts.itemHeight = listOpts.items[0].clientHeight;
+//   listOpts.itemCount = listOpts.items.length;
+  
+//   if (!itemsMax) {
+//     itemsMax = listOpts.itemCount;
+//   }
+  
+//   if (scrollSmooth) {
+//     var seamLessScrollPoint = ((itemsMax - 3) * listOpts.itemHeight);
+//     scrollW.scrollTop = seamLessScrollPoint;
+//   }      
+  
+// }
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   initItems();
+//   scrollW.onscroll = scrollWrap;
+// });
